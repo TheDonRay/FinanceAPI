9632cab337f11a7a2831239623a62e7a3b746b86
Rayat Chowdhury <rayatchowdhury2005@gmail.com>
2025-12-02 00:08:44 -0500

    working on this finance api

---
+++ backend/routes/apicall.js
@@ -0,0 +1,10 @@
+const express = require('express'); 
+const apicall = express.Router(); 
+
+//for this route I will be using the yahoo finance api call  
+//TODO set this route as an async route 
+apicall.get('stockData', (req, res) => { 
+    res.send('This route will retrieve stock data using yahoo finance api call.');
+}); 
+
+module.exports = apicall; 
--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,21 @@
-
 require('dotenv').config(); 
 const express = require('express'); 
 const app = express();   
 // set up cors here as such 
 const cors = require('cors'); 
 
-app.use(cors);  
+app.use(cors());   
+
+//import routes 
+const apiCall = require('./routes/apicall.js'); 
 
 // set up middleware here as such 
-app.use(express.json());   
+app.use(express.json());    
 
-const PORT = process.env.PORT; 
+const PORT = process.env.PORT;  
+
+//call the routes here as such 
+app.use('/api/', apiCall); 
 
 
 //set up simple backend route here as such 
